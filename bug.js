ceafce350ea63b3076c392e4084ae3ad01670100
zevineumann <65773211+zevineumann@users.noreply.github.com>
2020-06-24 22:01:43 +0300

    MoveButton

--- bug.js
+++ bug.js
@@ -1,9 +1,16 @@
+
 $(document).ready(function(){
 	$(".c").click(function(){
 		setInterval(zevi),1;
+	});
+	
+	$("#MoveButton").mouseover(function(){
+		var random = Math.floor(Math.random() * 500) -500;
+		document.getElementById('MoveButton').style.marginTop = random + "px";
 	});
 });
 
 function zevi() {
 	alert("ha ha try to take me off");
 }
+
