1f1019876684c6390954e6f7792b672d3b621c40
zevineumann <65773211+zevineumann@users.noreply.github.com>
2020-06-28 19:35:48 +0300

    Update bug.js

--- bug.js
+++ bug.js
@@ -1,12 +1,12 @@
 
 $(document).ready(function(){
-	$(".c").click(function(){
+	$(".bug").click(function(){
 		setInterval(zevi),1;
 	});
 	
 	$("#MoveButton").mouseover(function(){
 		var randomtop = Math.floor(Math.random() * 400) -500;
-		var randomleft = Math.floor(Math.random() * 400);
+		var randomleft = Math.floor(Math.random() * 800);
 		document.getElementById('MoveButton').style.marginTop = randomtop + "px";
 		document.getElementById('MoveButton').style.marginLeft = randomleft + "px";
 	});
