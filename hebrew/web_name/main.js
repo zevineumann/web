41b2647cfb1fa6d48d4236b468c3b21ab5bde792
zevineumann <65773211+zevineumann@users.noreply.github.com>
2020-11-15 19:10:10 +0200

    Update main.js

--- hebrew/web_name/main.js
+++ hebrew/web_name/main.js
@@ -6,7 +6,7 @@ function calculateBDay(year, month, day)  {
 	var res = Math.floor((now - bday) / (1000*60*60*24*365));
 	var ress = Math.floor((now - bday) / (1000*60));
 	ress = ress.toLocaleString('en');	
-	document.getElementById("bday").innerHTML ="גיל: "+res+"<br>הדקות שבזבזת כול חיך: "+ress;
+	document.getElementById("bday").innerHTML ="גיל: "+res+"<br>הדקות שבזבזת כל חייך: "+ress;
 
 	var url = new URL(window.location.href);
 	document.getElementById("name").innerHTML ="שמך: "+url.searchParams.get('fname')+" "+ url.searchParams.get('lname');
