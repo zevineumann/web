6f8a2509691c8e15ed22c743a7fed34606864e99
zevineumann <65773211+zevineumann@users.noreply.github.com>
2020-11-15 18:46:48 +0200

    hebrew

---
+++ hebrew/LINE_FRONT/game_over/game_over.js
@@ -0,0 +1,7 @@
+
+
+$(document).ready(function(){
+	let params = (new URL(document.location)).searchParams;
+	let score = params.get('score');
+	document.getElementById("score").innerHTML = score+":נקודות";
+});
---
+++ hebrew/bug.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function(){
+	$(".bug").click(function(){
+		setInterval(zevi),1;
+	});
+	
+	$("#MoveButton").mouseover(function(){
+		var randomtop = Math.floor(Math.random() * 400) -500;
+		var randomleft = Math.floor(Math.random() * 800);
+		document.getElementById('MoveButton').style.marginTop = randomtop + "px";
+		document.getElementById('MoveButton').style.marginLeft = randomleft + "px";
+	});
+});
+
+function zevi() {
+	alert("ha ha try to take me off");
+}
+
